a614eb228e5e0e0daafb33f4e3a5dafcd17e2558
robin-plancon <robin.plancon@outlook.com>
2023-09-21 15:02:26 +0200

    feat: A simple footer

---
+++ src/components/utils/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.scss';
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<a href="/about" className="footer-link">
+				A propos
+			</a>
+			<p className="footer-content">2023 RetrO&apos;Game Collection</p>
+		</footer>
+	);
+}
+
+export default Footer;
