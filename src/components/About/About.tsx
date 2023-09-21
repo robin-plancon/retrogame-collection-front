8e0ac9046c494b3e479a889d97eb6021a1b973f1
robin-plancon <robin.plancon@outlook.com>
2023-09-21 14:00:27 +0200

    feat: add parameters to TeamCard component

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,14 @@
 import './About.scss';
 
-import avatar from '../../../resources/avatar.jpg';
+import florian from '../../assets/avatar/florian.jpg';
+import laetitia from '../../assets/avatar/laetitia.jpg';
+import lionel from '../../assets/avatar/lionel.jpg';
+import robin from '../../assets/avatar/robin.jpg';
+import vanessa from '../../assets/avatar/vanessa.jpg';
 import TeamCard from './TeamCard/TeamCard';
 
+// About component.
+// This component is used in /about route. It displays a description of the project and the team members.
 function About() {
 	return (
 		<div className="about">
@@ -15,11 +21,15 @@ function About() {
 			</p>
 			<h2 className="about-title">L&apos;équipe</h2>
 			<div className="about-team">
-				<TeamCard avatar={avatar} />
-				<TeamCard avatar={avatar} />
-				<TeamCard avatar={avatar} />
-				<TeamCard avatar={avatar} />
-				<TeamCard avatar={avatar} />
+				<TeamCard avatar={robin} name="Robin Plançon" projectRole="Product Owner" />
+				<TeamCard
+					avatar={laetitia}
+					name="Laetitia Phommarath"
+					projectRole="Lead Dev front"
+				/>
+				<TeamCard avatar={vanessa} name="Vanessa Slama" projectRole="Scrum Master" />
+				<TeamCard avatar={lionel} name="Lionel Bouldoyré" projectRole="Git Master" />
+				<TeamCard avatar={florian} name="Florian Corlu" projectRole="Lead Dev Back" />
 			</div>
 		</div>
 	);
--- src/components/About/TeamCard/TeamCard.tsx
+++ src/components/About/TeamCard/TeamCard.tsx
@@ -2,14 +2,18 @@ import './TeamCard.scss';
 
 interface TeamCardProps {
 	avatar: string;
+	name: string;
+	projectRole: string;
 }
 
-function TeamCard({ avatar }: TeamCardProps) {
+// TeamCard component.
+// This component is used in About.tsx. It displays a team member's avatar, name and project role.
+function TeamCard({ avatar, name, projectRole }: TeamCardProps) {
 	return (
 		<div className="team-card">
-			<img src={avatar} alt="Avatar de John" className="team-card--image" />
-			<h3 className="team-card--name">John</h3>
-			<p className="team-card--role">Product Owner</p>
+			<img src={avatar} alt={`avatar de ${name}`} className="team-card--image" />
+			<h3 className="team-card--name">{name}</h3>
+			<p className="team-card--role">{projectRole}</p>
 		</div>
 	);
 }
