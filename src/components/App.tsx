026e1ce9386824e6dd000f464874fe2107cdd9c6
robin-plancon <robin.plancon@outlook.com>
2023-09-21 16:20:40 +0200

    feat: A simple header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,10 +6,12 @@ import { Route, Routes } from 'react-router-dom';
 import About from './About/About';
 import Home from './Home/Home';
 import Footer from './utils/Footer/Footer';
+import Header from './utils/Header/Header';
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
