73ee7acff841b98c5f7f174ba660cb27a0f919c9
Robin <robin.plancon@outlook.com>
2023-09-25 10:48:38 +0200

    style: create responsive design

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -3,17 +3,32 @@ import './Signup.scss';
 function Signup() {
 	return (
 		<div className="signup">
-			<h1 className="signup-title">Signup</h1>
+			<h1 className="signup-title">Inscription</h1>
 			<form className="signup-form" /* onSubmit={handleSubmit} */>
-				<label htmlFor="pseudo">Pseudo</label>
-				<input type="text" id="pseudo" name="pseudo" />
-				<label htmlFor="email">Email</label>
-				<input type="email" id="email" name="email" />
-				<label htmlFor="password">Mot de passe</label>
-				<input type="password" id="password" name="password" />
-				<label htmlFor="confirmPassword">Confirmer le mot de passe</label>
-				<input type="password" id="confirmPassword" name="confirmPassword" />
-				<button type="submit">Créer un compte</button>
+				<label htmlFor="pseudo" className="signup-label">
+					Pseudo
+				</label>
+				<input type="text" id="pseudo" name="pseudo" className="signup-input" />
+				<label htmlFor="email" className="signup-label">
+					Email
+				</label>
+				<input type="email" id="email" name="email" className="signup-input" />
+				<label htmlFor="password" className="signup-label">
+					Mot de passe
+				</label>
+				<input type="password" id="password" name="password" className="signup-input" />
+				<label htmlFor="confirmPassword" className="signup-label">
+					Confirmer le mot de passe
+				</label>
+				<input
+					type="password"
+					id="confirmPassword"
+					name="confirmPassword"
+					className="signup-input"
+				/>
+				<button type="submit" className="signup-button">
+					Créer un compte
+				</button>
 			</form>
 		</div>
 	);
