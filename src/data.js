0826e78f5688a710442bfbe12ed4f9fe149f18bb
LaetitiaPhommarath <hey.lili@hotmail.fr>
2023-09-25 15:20:08 +0200

    Add Game data + game card components

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const gameData = [
+	{
+		id: 1,
+		title: 'The Legend of Zelda: Ocarina of Time',
+		console: 'Nintendo 64',
+		genre: 'Action-Adventure',
+		releaseDate: '1998',
+		imageUrl:
+			'https://www.nme.com/wp-content/uploads/2021/07/The-Legend-Of-Zelda-Ocarina-Of-Time-Credit-Nintendo-1@2000x1270-1392x884.jpg',
+	},
+
+	{
+		id: 2,
+		title: 'Final Fantasy VII',
+		console: 'Nintendo 64',
+		genre: 'RPG',
+		releaseDate: '1997',
+		imageUrl:
+			'https://image.api.playstation.com/vulcan/img/cfn/113075PxnarzRek4cRpjrRWSpLfrcBd23B5e_Yj2azms6nWYKmySv4h3a22G5_R1CM4BQUmSRD6oOArDROKv041NUkgun78-.png',
+	},
+
+	{
+		id: 3,
+		title: 'Super Mario 64',
+		console: 'Nintendo 64',
+		genre: 'Plate-forme',
+		releaseDate: '1997',
+		imageUrl: 'https://i.ebayimg.com/images/g/h3cAAOSwRxNkYCy9/s-l1200.jpg',
+	},
+
+	{
+		id: 4,
+		title: 'Super Mario 64',
+		console: 'Nintendo 64',
+		genre: 'Plate-forme',
+		releaseDate: '1997',
+		imageUrl: 'https://image.jeuxvideo.com/images/ps/m/g/mgsops0f.jpg',
+	},
+];
+export default gameData;
