7a23142ce06d0096bf61ef58eba08847a7b2f322
robin-plancon <robin.plancon@outlook.com>
2023-09-26 11:22:18 +0200

    feat: add fields and styles

--- src/components/Signin/Signin.tsx
+++ src/components/Signin/Signin.tsx
@@ -24,10 +24,48 @@ function Signin() {
 	return (
 		<div className="signin">
 			<h1 className="signin-title">Connexion</h1>
-			<form className="signup-title" onSubmit={handleSubmit(onSubmit)}>
+			<form className="signin-form" onSubmit={handleSubmit(onSubmit)}>
 				<label htmlFor="email" className="signin-label">
 					Email
 				</label>
+				<input
+					type="email"
+					id="email"
+					className="signin-input"
+					{...register('email', {
+						required: 'Ce champ est requis',
+						pattern: {
+							value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+							message: 'Adresse email invalide',
+						},
+					})}
+				/>
+				{errors.email && (
+					<span className="signin-error">{errors.email.message as string}</span>
+				)}
+				<label htmlFor="password" className="signin-label">
+					Mot de passe
+				</label>
+				<input
+					type="password"
+					id="password"
+					className="signin-input"
+					{...register('password', {
+						required: 'Ce champ est requis',
+						minLength: {
+							value: 6,
+							message: 'Le mot de passe doit contenir au moins 6 caractères',
+						},
+						pattern: {
+							value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/i,
+							message:
+								'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial',
+						},
+					})}
+				/>
+				{errors.password && (
+					<span className="signin-error">{errors.password.message as string}</span>
+				)}
 			</form>
 		</div>
 	);
