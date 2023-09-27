2192ef5f28ac5b91f39742260c7ebb202d32c6cb
LaetitiaPhommarath <hey.lili@hotmail.fr>
2023-09-28 01:39:49 +0200

    Adding pagination

--- src/data.ts
+++ src/data.ts
@@ -25,24 +25,26 @@ const gameData = [
 
 	{
 		id: 3,
-		title: 'Super Mario 64',
-		console: 'Nintendo 64',
-		genre: 'Plate-forme',
-		releaseDate: '1997',
-		imageUrl: 'https://i.ebayimg.com/images/g/h3cAAOSwRxNkYCy9/s-l1200.jpg',
+		title: 'Sonic the Hedgehog',
+		console: 'Sega Genesis',
+		genre: 'Plateforme',
+		releaseDate: '1991',
+		imageUrl:
+			'https://d.newsweek.com/en/full/2018092/sonic-hedgehog-2.jpg?w=1600&h=1600&q=88&f=ef770fd1a6192ca097d943b0ba9c484f',
 		summary:
-			"Il est le premier jeu de la série entièrement en 3D temps réel et propose à ce titre une liberté totale de mouvement, de larges niveaux ouverts, et une 3D polygonale en contraste avec les sprites en 2D des Mario précédents. Il conserve également de nombreux éléments du gameplay et de l'univers des précédents volets.",
+			'Sonic the Hedgehog est un jeu de plateforme rapide mettant en scène le hérisson bleu Sonic dans sa quête pour arrêter le méchant Dr. Robotnik. Le jeu est célèbre pour sa vitesse et ses niveaux colorés.',
 	},
 
 	{
 		id: 4,
-		title: 'Metal Gear Solid',
-		console: 'Playstation',
-		genre: 'Action',
-		releaseDate: '1998',
-		imageUrl: 'https://image.jeuxvideo.com/images/ps/m/g/mgsops0f.jpg',
+		title: 'Street Fighter II: The World Warrior',
+		console: 'Super Nintendo Entertainment System (SNES)',
+		genre: 'Combat',
+		releaseDate: '1992',
+		imageUrl:
+			'https://i.discogs.com/B58zXRK4-ijLglLLGIYm-eZbpCxBgo9gByCEz8qUh8k/rs:fit/g:sm/q:90/h:598/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQzNTA5/MTgtMTU5MDk0ODUw/Ni0zNDcwLmpwZWc.jpeg',
 		summary:
-			"L'histoire relate la mission d'infiltration qui deviendra par la suite « l'incident de Shadow Moses ». Non seulement le secret entourant la naissance de Solid Snake est révélé, mais son frère fait également son apparition : Liquid Snake, au patrimoine génétique identique.",
+			"Street Fighter II propose huit personnages jouables de base et sept styles de combat différents, un système de jeu novateur, des graphismes et une bande sonore riche et variée. Street Fighter II est considéré comme l'une des références des jeux de combat en un contre un, en plus d'en être l'un des pionniers.",
 	},
 
 	{
@@ -58,14 +60,59 @@ const gameData = [
 
 	{
 		id: 6,
-		title: 'Street Fighter II: The World Warrior',
-		console: 'Super Nintendo Entertainment System (SNES)',
-		genre: 'Combat',
-		releaseDate: '1992',
-		imageUrl:
-			'https://i.discogs.com/B58zXRK4-ijLglLLGIYm-eZbpCxBgo9gByCEz8qUh8k/rs:fit/g:sm/q:90/h:598/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQzNTA5/MTgtMTU5MDk0ODUw/Ni0zNDcwLmpwZWc.jpeg',
+		title: 'Metal Gear Solid',
+		console: 'Playstation',
+		genre: 'Action',
+		releaseDate: '1998',
+		imageUrl: 'https://image.jeuxvideo.com/images/ps/m/g/mgsops0f.jpg',
 		summary:
-			"Street Fighter II propose huit personnages jouables de base et sept styles de combat différents, un système de jeu novateur, des graphismes et une bande sonore riche et variée. Street Fighter II est considéré comme l'une des références des jeux de combat en un contre un, en plus d'en être l'un des pionniers.",
+			"L'histoire relate la mission d'infiltration qui deviendra par la suite « l'incident de Shadow Moses ». Non seulement le secret entourant la naissance de Solid Snake est révélé, mais son frère fait également son apparition : Liquid Snake, au patrimoine génétique identique.",
+	},
+
+	{
+		id: 7,
+		title: 'Super Mario 64',
+		console: 'Nintendo 64',
+		genre: 'Plate-forme',
+		releaseDate: '1997',
+		imageUrl: 'https://i.ebayimg.com/images/g/h3cAAOSwRxNkYCy9/s-l1200.jpg',
+		summary:
+			"Il est le premier jeu de la série entièrement en 3D temps réel et propose à ce titre une liberté totale de mouvement, de larges niveaux ouverts, et une 3D polygonale en contraste avec les sprites en 2D des Mario précédents. Il conserve également de nombreux éléments du gameplay et de l'univers des précédents volets.",
+	},
+
+	{
+		id: 8,
+		title: 'Castlevania',
+		console: 'Nintendo Entertainment System (NES)',
+		genre: 'Action/Aventure',
+		releaseDate: '1987',
+		imageUrl: 'https://m.media-amazon.com/images/I/5156i7MM6hL._AC_UF1000,1000_QL80_.jpg',
+		summary:
+			"Castlevania suit les aventures du chasseur de vampires Simon Belmont alors qu'il explore le sinistre château de Dracula. Le jeu est connu pour son atmosphère sombre et son gameplay exigeant. Les joueurs doivent combattre des monstres et des boss pour vaincre Dracula.",
+	},
+
+	{
+		id: 9,
+		title: 'Donkey Kong',
+		console: 'Nintendo Entertainment System',
+		genre: 'Plateforme',
+		releaseDate: '1986',
+		imageUrl:
+			'https://proxy.olhardigital.com.br/wp-content/uploads/2020/07/20200715124525.jpg',
+		summary:
+			'Donkey Kong est un jeu de plateforme classique qui met en scène Jumpman (qui deviendra plus tard Mario) tentant de sauver la demoiselle en détresse, Pauline, des griffes de Donkey Kong. Le jeu a introduit certains des personnages les plus emblématiques de Nintendo et a contribué à lancer la carrière de Mario.',
+	},
+
+	{
+		id: 10,
+		title: 'Mega Man',
+		console: 'Nintendo Entertainment System (NES)',
+		genre: 'Action/Plateforme',
+		releaseDate: '1987',
+		imageUrl:
+			'https://images.laprovence.com/media/2017/12/05/megaman11_art_1512379726.png?twic=v1/crop=1331x732@83x0/cover=1960x1103',
+		summary:
+			"Mega Man, également connu sous le nom de Rockman au Japon, est un jeu d'action-plateforme où les joueurs incarnent Mega Man, un robot capable de copier les pouvoirs de ses ennemis vaincus. Le jeu est célèbre pour sa difficulté, ses combats de boss mémorables et ses nombreux titres de la série.",
 	},
 ];
 
