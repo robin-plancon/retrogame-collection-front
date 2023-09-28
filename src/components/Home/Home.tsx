3051dd7680743d255cdb09851a6fe19ed95485e6
LaetitiaPhommarath <hey.lili@hotmail.fr>
2023-09-28 23:39:55 +0200

    Home filter

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,36 +1,38 @@
 import '../GameCard/GameCard.scss';
 import './Home.scss';
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import gameData from '../../data';
-import { useAppDispatch } from '../../hooks/redux';
-import { getGames } from '../../store/reducers/game';
 import GameCard from '../GameCard/GameCard';
+import Filter from './Filter/Filter';
 
 function Home() {
-	const [isFirst, setIsFirst] = useState(true);
+	const [visibleGames, setVisibleGames] = useState(4); // Number of cards to display initially
+	const gamesToShow = gameData.slice(0, visibleGames);
 
-	const dispacth = useAppDispatch();
-
-	useEffect(() => {
-		if (isFirst) {
-			setIsFirst(false);
-			return;
-		}
-		dispacth(getGames());
-	}, [isFirst]);
+	const handleShowMore = () => {
+		setVisibleGames(visibleGames + 4); // + 4 more games
+	};
 
 	return (
 		<div className="body">
+			<div className="filter-menu">
+				<Filter />
+			</div>
 			<div className="game-list">
-				{gameData.map((game) => (
+				{gamesToShow.map((game) => (
 					<Link key={game.id} to={`/game/${game.id}`} state={{ summary: game.summary }}>
 						<GameCard game={game} />
 					</Link>
 				))}
 			</div>
+			{visibleGames < gameData.length && (
+				<div className="load-more">
+					<button onClick={handleShowMore}>Afficher plus</button>
+				</div>
+			)}
 		</div>
 	);
 }
