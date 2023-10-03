49cc1aab9ece8d1c3722558912d2e98f1003da5b
robin-plancon <robin.plancon@outlook.com>
2023-10-03 15:06:24 +0200

    feat: add button add/remove game to collection on detail page

--- src/components/GameDetails/GameDetails.tsx
+++ src/components/GameDetails/GameDetails.tsx
@@ -6,12 +6,18 @@ import { useParams } from 'react-router-dom';
 
 import placeholder from '../../assets/placeholder_image.png';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import {
+	addGameToCollection,
+	removeGameFromCollection,
+} from '../../store/reducers/collection';
 import { getGameBySlug } from '../../store/reducers/game';
 
 function GameDetails() {
 	const { slug } = useParams<string>();
 
 	const gameData = useAppSelector((state) => state.games.games);
+	const isAuth = useAppSelector((state) => state.auth);
+	const collection = useAppSelector((state) => state.collection.games);
 
 	const dispatch = useAppDispatch();
 
@@ -23,6 +29,19 @@ function GameDetails() {
 			dispatch(getGameBySlug(slug));
 		}
 	}, [game]);
+
+	const handleClick = () => {
+		if (!game) {
+			return;
+		}
+		if (collection.find((g) => g.id === game.id)) {
+			// Remove game from collection
+			dispatch(removeGameFromCollection(game));
+		} else {
+			// Add game to collection
+			dispatch(addGameToCollection(game));
+		}
+	};
 
 	if (!game && slug) {
 		return <div>Loading...</div>;
@@ -44,26 +63,35 @@ function GameDetails() {
 	const genres = game.genres?.map((genre) => genre.name).join(', ') || 'non renseigné';
 
 	return (
-		<Link to="/" className="game-details">
-			<img
-				src={game.cover?.url || placeholder}
-				alt={game.name}
-				className="game-details--image"
-			/>
-			<div className="game-details--info">
-				<h2 className="game-details--name">{game.name}</h2>
-				<p className="game-details--label">Console:</p>
-				<p className="game-details--value">{platforms}</p>
-				<p className="game-details--label">Genre:</p>
-				<p className="game-details--value">{genres}</p>
-				<p className="game-details--label">Date de sortie:</p>
-				<p className="game-details--value">{formattedDate}</p>
-				<p className="game-details--label">Description:</p>
-				<p className="game-details--description">
-					{game.summary || 'résumé non disponible'}
-				</p>
-			</div>
-		</Link>
+		<div className="game-details">
+			<Link to="/" className="game-details--game">
+				<img
+					src={game.cover?.url || placeholder}
+					alt={game.name}
+					className="game-details--image"
+				/>
+				<div className="game-details--info">
+					<h2 className="game-details--name">{game.name}</h2>
+					<p className="game-details--label">Console:</p>
+					<p className="game-details--value">{platforms}</p>
+					<p className="game-details--label">Genre:</p>
+					<p className="game-details--value">{genres}</p>
+					<p className="game-details--label">Date de sortie:</p>
+					<p className="game-details--value">{formattedDate}</p>
+					<p className="game-details--label">Description:</p>
+					<p className="game-details--description">
+						{game.summary || 'résumé non disponible'}
+					</p>
+				</div>
+			</Link>
+			{isAuth.user && isAuth.token && (
+				<button className="game-card--button" onClick={handleClick}>
+					{collection.find((g) => g.id === game.id)
+						? 'Retirer de ma collection'
+						: 'Ajouter à ma collection'}
+				</button>
+			)}
+		</div>
 	);
 }
 
