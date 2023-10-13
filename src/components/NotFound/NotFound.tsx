c74f446fd4314d45efbfdd4302c1d30b0cfdba9a
LaetitiaPhommarath <hey.lili@hotmail.fr>
2023-10-13 14:59:56 +0200

    css adjustments and pictures added to 404 page

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,13 +1,33 @@
 import './NotFound.scss';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import notFoundImage from '../../assets/NotFound/404.gif';
+import errorGif from '../../assets/NotFound/404.gif';
+import notFound from '../../assets/NotFound/notfound.jpg';
+import errorPage from '../../assets/NotFound/notfound1.jpeg';
+
+const images = {
+	notFound: notFound,
+	error: errorGif,
+	errorPage: errorPage,
+};
+
+const getRandomImage = () => {
+	const imageKeys = Object.keys(images);
+	const randomIndex = Math.floor(Math.random() * imageKeys.length);
+	return imageKeys[randomIndex];
+};
 
 const NotFoundPage = () => {
+	const [randomImage, setRandomImage] = useState(getRandomImage());
+
+	useEffect(() => {
+		setRandomImage(getRandomImage());
+	}, []);
+
 	return (
 		<div className="not-found-container">
-			<img src={notFoundImage} alt="Page non trouvée" className="not-found-image" />
+			<img src={images[randomImage]} alt="Page non trouvée" className="not-found-image" />
 		</div>
 	);
 };
