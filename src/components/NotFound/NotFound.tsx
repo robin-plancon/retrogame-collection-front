af7eafba3079725cc92dd53c52943e4439bee401
LaetitiaPhommarath <hey.lili@hotmail.fr>
2023-10-13 16:16:31 +0200

    css adjustments

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,33 +1,13 @@
 import './NotFound.scss';
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import errorGif from '../../assets/NotFound/404.gif';
-import notFound from '../../assets/NotFound/notfound.jpg';
-import errorPage from '../../assets/NotFound/notfound1.jpeg';
-
-const images = {
-	notFound: notFound,
-	error: errorGif,
-	errorPage: errorPage,
-};
-
-const getRandomImage = () => {
-	const imageKeys = Object.keys(images);
-	const randomIndex = Math.floor(Math.random() * imageKeys.length);
-	return imageKeys[randomIndex];
-};
+import notFoundImage from '../../assets/NotFound/404.gif';
 
 const NotFoundPage = () => {
-	const [randomImage, setRandomImage] = useState(getRandomImage());
-
-	useEffect(() => {
-		setRandomImage(getRandomImage());
-	}, []);
-
 	return (
 		<div className="not-found-container">
-			<img src={images[randomImage]} alt="Page non trouvée" className="not-found-image" />
+			<img src={notFoundImage} alt="Page non trouvée" className="not-found-image" />
 		</div>
 	);
 };
